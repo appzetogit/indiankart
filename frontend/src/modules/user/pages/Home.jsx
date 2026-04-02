c6637aa4efe34df94bf1c296fcc5cd1783655fc0
Aditi Parihar <aditiparihar179@gmail.com>
2026-04-02 16:46:39 +0530

    fix

--- frontend/src/modules/user/pages/Home.jsx
+++ frontend/src/modules/user/pages/Home.jsx
@@ -3,7 +3,7 @@ import CategoryLandingSections from '../components/category/CategoryLandingSecti
 
 const Home = () => {
     return (
-        <div className="bg-white pt-2 flex-1 flex flex-col">
+        <div className="bg-white pt-2 pb-[calc(108px+env(safe-area-inset-bottom))] md:pb-0 flex-1 flex flex-col">
             <CategoryLandingSections categoryName="For You" />
         </div>
     );
